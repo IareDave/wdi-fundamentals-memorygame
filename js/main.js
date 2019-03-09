307b16952c57393f1db8b2754486e9ff796ab5b3
Dave <gouletdavid95@gmail.com>
2019-03-08 19:11:01 -0500

    created game logic. missing steps 8 to 10

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
 console.log("User flipped " + cardOne);
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("the card flipped correctly" + cardsInPlay);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("here is the length of array" + cardsInPlay.length);
